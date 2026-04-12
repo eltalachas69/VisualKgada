a6aeaf4c3aaccfde011968562aa6903ea0c3a086
ElCuilango <juancrls760@gmail.com>
2026-04-12 14:52:47 -0600

    Cambio de nombre

---
+++ Gym/Gym/Gym/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gym.View_Model
+{
+    public class ViewModelCommand : ICommand
+    {
+        //Representa el metodo que se ejecuta 
+        //cuando se llama Execute
+        private readonly Action<object> _executeAction;
+
+        //Representa una funcion que devuelve bool
+        //y nos dice si el comando se puede ejecutar o no
+        private readonly Predicate<object> _canExecuteAction;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
